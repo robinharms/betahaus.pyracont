86315ab3aac62d710216a380addcfa649bad1990
Fredrik Nilson <sassur@gmail.com>
2012-02-13 15:07:35 +0100

    adding title and description from decorator to schema

--- betahaus/pyracont/tests/fixtures/schemas.py
+++ betahaus/pyracont/tests/fixtures/schemas.py
@@ -3,6 +3,6 @@ import colander
 from betahaus.pyracont.decorators import schema_factory
 
 
-@schema_factory('DummySchema')
+@schema_factory('DummySchema', title=u"dummy schema title", description=u"dummy schema description")
 class DummySchema(colander.Schema):
     dummy_schema_node = colander.SchemaNode(colander.String())
